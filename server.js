511375e00037d7cb16e390b257fcb1b5c658dc20
Parr <tedblount01@gmail.com>
2020-07-09 16:19:32 -0500

    removed cookie-parser wasnt being used

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const next = require('next');
 const express = require('express');
-const cookieParser = require('cookie-parser');
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
@@ -13,7 +12,6 @@ app.prepare().then(() => {
     const server = express();
 
     server.use(express.json());
-    server.use(cookieParser(COOKIE_SECRET))
 
 
     server.get('*', (req, res) => {
