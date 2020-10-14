f0f2aa650f3f3d22d0c9d672531b5aa9cb92f73e
Parr <tedblount01@gmail.com>
2020-10-14 11:28:58 -0500

    2nd vid

--- pages/videos.js
+++ pages/videos.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout";
 export default () => (
     <Layout title="Videos">
         <video src="/static/hairyspidez0001-0120.mkv" autoPlay loop></video>
+        <video src="/static/AAH120001-0120.mkv" autoPlay loop></video>
 
 <style jsx>{`
 video {
